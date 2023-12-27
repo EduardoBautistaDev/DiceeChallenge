a04679b31334224b9672c7f6a70b810b012a1a8f
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2023-12-27 15:22:38 -0600

    finished

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var diceImages = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png'];
-var diceImageElement = document.querySelector("img1");
-diceImageElement.setAttribute('src', diceImages[randomNumber1 - 1]);
+var diceImageElement1 = document.querySelector(".img1");
+var diceImageElement2 = document.querySelector(".img2");
+var titleElement = document.querySelector("h1");
+
+diceImageElement1.setAttribute('src', "images/" + diceImages[randomNumber1 - 1]);
+diceImageElement2.setAttribute('src', "images/" + diceImages[randomNumber2 - 1]);
+
+if (randomNumber1 > randomNumber2) {
+    titleElement.innerText = "🚩 Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    titleElement.innerText = "Player 2 wins! 🚩"
+}
+else{
+    titleElement.innerText = "🚩 Draw! 🚩"
+}
