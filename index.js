de60f2be1059fd4e18dd0f3c00664757a3dd153c
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2023-12-27 15:11:06 -0600

    added first rand dice

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
+var diceImages = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png'];
+var diceImageElement = document.querySelector("img1");
+diceImageElement.setAttribute('src', diceImages[randomNumber1 - 1]);
